24e48e0490b91d948afd42e923caa14d73054b27
xxxsssxxx <s.klymoshenko@gmail.com>
2018-06-27 19:11:23 +0200

    v.1

--- main.js
+++ main.js
@@ -2,36 +2,49 @@
      el: '#app',
      data: {
          title: ' Count Your Coffe Price ',
-      	price: 0,
+      	 totalPrice: 0,
          items: [{
                  name: "Espresso",
                  types: [{
                      name: 'small',
-                     quantity: "0"
+                     quantity: 0,
+                     price: 1.50
                  }]
              },
              {
                  name: "Latte",
-                 types: [{ name: 'small', quantity: "0", },
-                     { name: 'medium', quantity: "0", },
-                     { name: 'large', quantity: "0", }
+                 types: [{ name: 'small', quantity: 0,price: 1.99 },
+                     { name: 'medium', quantity: 0,price: 2.99 },
+                     { name: 'large', quantity: 0,price: 3.99 }
                  ]
              },
              {
                  name: "Cappucino",
-                 types: [{ name: 'small', quantity: "0" },
-                     { name: 'medium', quantity: "0" },
-                     { name: 'large', quantity: "0" }
+                 types: [{ name: 'small', quantity: 0,  price: 1.75},
+                     { name: 'medium', quantity: 0,  price: 2.75},
+                     { name: 'large', quantity: 0 , price: 3.75 }
                  ]
              }
          ]
      },
      methods: {
+         countTotalAdd(par, par2){
+         	let uroundedPrice =  this.totalPrice + this.items[par].types[par2].price
+         	this.totalPrice = (+uroundedPrice.toFixed(10))
+
+         },
          addCoffe(par, par2) {
             this.items[par].types[par2].quantity++
+            this.countTotalAdd(par,par2)
+         },
+         countTotalDel(par, par2){
+         	let uroundedPrice =  this.totalPrice - this.items[par].types[par2].price
+         	this.totalPrice = (+uroundedPrice.toFixed(10))
+
          },
          deleteCoffe(par, par2){
          	this.items[par].types[par2].quantity--
+         	this.countTotalDel(par,par2)
          }
 
      }
